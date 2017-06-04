9b2388325dc5ae2edbb533c9559af8c05c4c61a8
Kishy-nivas <kishynivas10@gmail.com>
2017-06-04 23:11:28 +0530

    Added a new function to the class Question

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+
+# Here we are creating two models one is for Questions  and another for Choices
+# each Question contains a text and pub_date and Choices contains a text and number of votes
+# Each choice is  related  to a single question
+class  Question(models.Model):
+    question_text = models.CharField(max_length =20)
+    pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() -datetime.timedelta(days =1 )
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question,on_delete = models.CASCADE)
+    choice_text = models.CharField(max_length =8)
+    votes = models.IntegerField(default =0 )
