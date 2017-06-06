716f066dd72c8f3483fe5699b2fc543930247b80
Kishy-nivas <kishynivas10@gmail.com>
2017-06-06 21:11:11 +0530

    Done basic UI with good url mappin

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 urlpatterns =[
     url('^$',views.index,name ='index'),
-
+    url('^(?P<question_id>[0-9]+)/$',views.detail,name ='detail'),
+    url('^(?P<question_id>[0-9]+)/results/$',views.results,name= 'results'),
+    url('^(?P<question_id>[0-9]+)/votes/$',views.votes, name='votes'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from polls.models import Question,Choice
 
 # Create your views here.
 def index(request):
-    return HttpResponse("You are currently in index page ")
+    question_list = Question.objects.all()
+    return render(request,'polls/index.html',{'question_list':question_list})
+
+def results(request,question_id):
+    return HttpResponse("You are looking at results of %s" % question_id)
+
+
+def votes(request,question_id):
+    return HttpResponse("You are looking at votes for the question with id %s" % question_id)
+
+def detail(request,question_id):
+    return HttpResponse("You are looking at the details page %s" % question_id)
