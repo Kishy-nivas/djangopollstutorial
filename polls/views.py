263ffe3aa85b71ae5613f48bdba686cc3561939a
Kishy-nivas <kishynivas10@gmail.com>
2017-06-08 17:14:19 +0530

    Finished the tutorial except generic views,modifying the admin site.

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from polls.models import Question,Choice
+from django.core.urlresolvers import reverse
 
 # Create your views here.
 def index(request):
@@ -8,12 +9,21 @@ def index(request):
     return render(request,'polls/index.html',{'question_list':question_list})
 
 def results(request,question_id):
-    return HttpResponse("You are looking at results of %s" % question_id)
+    question_list = get_object_or_404(Question, pk= question_id)
+    return render(request,'polls/results.html',{'question_list':question_list})
 
 
 def votes(request,question_id):
-    return HttpResponse("You are looking at votes for the question with id %s" % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        return render(request,'polls/detail.html',{'question_detail':question,'error_msg':"You didn't enter a choice ",})
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args = (question_id,)))
 
 def detail(request,question_id):
-    question_detail = get_object_or_404(Question,id=question_id)
+    question_detail = get_object_or_404(Question,pk=question_id)
     return render(request,'polls/detail.html',{'question_detail':question_detail})
